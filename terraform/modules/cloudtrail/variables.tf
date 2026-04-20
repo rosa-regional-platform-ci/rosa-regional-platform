a959c188d633a89d68798aa74047133a38e5454c
Brian Smith <bsmit@redhat.com>
2026-04-20 16:04:56 -0400

    fix force destroy issue

--- terraform/modules/cloudtrail/variables.tf
+++ terraform/modules/cloudtrail/variables.tf
@@ -1,3 +1,9 @@
+variable "force_destroy" {
+  description = "Allow the S3 bucket to be destroyed even if it contains objects. Enable only for ephemeral environments."
+  type        = bool
+  default     = false
+}
+
 variable "cluster_id" {
   description = "Unique cluster identifier used as a name prefix for all resources"
   type        = string
