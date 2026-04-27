8cd945e6d0cdf05becf7830539821c9600f7fcd9
Kirk Bater <kirk.bater@gmail.com>
2026-04-27 10:51:25 -0400

    adds feature flag to Cloudtrail configuration

--- terraform/config/regional-cluster/main.tf
+++ terraform/config/regional-cluster/main.tf
@@ -258,6 +258,7 @@ module "hyperfleet_infrastructure" {
 # =============================================================================
 
 module "cloudtrail" {
+  count  = var.enable_cloudtrail ? 1 : 0
   source = "../../modules/cloudtrail"
 
   cluster_id  = var.regional_id
